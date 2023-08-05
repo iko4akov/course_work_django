e71837aa34a81625d7ef9dca2544d90926f1ddcf
iko4akov <iko4akov@gmail.com>
2023-08-05 16:11:50 +0300

    complete task1

---
+++ services/send_email/send_email.py
@@ -0,0 +1,17 @@
+from django.core.mail import send_mail
+
+from config.settings import EMAIL_HOST_USER
+from send.models import Send
+
+
+def send_email_(send_item: Send):
+    print(send_item.message.theme)
+    print(send_item.message.message)
+    print(EMAIL_HOST_USER)
+    print(send_item.client.email)
+    send_mail(
+        send_item.message.theme,
+        send_item.message.message,
+        EMAIL_HOST_USER,
+        [EMAIL_HOST_USER],
+    )
