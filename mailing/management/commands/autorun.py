703a3d63ace6386196f1d0a72ffc0ea19eda1360
iko4akov <iko4akov@gmail.com>
2023-09-05 18:55:37 +0300

    cron comlete

--- mailing/management/commands/autorun.py
+++ mailing/management/commands/autorun.py
@@ -1,4 +1,8 @@
+from django.core.management import BaseCommand
+
 from services.run_mailer import run_mailer
 
-def handle():
-    run_mailer()
+class Command(BaseCommand):
+
+    def handle(self, *args, **kwargs):
+        run_mailer()
